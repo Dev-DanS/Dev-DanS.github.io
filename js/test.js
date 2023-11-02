3679cfa99ea32c384f55c0aec21cf17b2353ff07
Dan <146542914+Dev-DanS@users.noreply.github.com>
2023-11-02 19:04:06 +0800

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -78,3 +78,20 @@ var watchId = navigator.geolocation.watchPosition(
   handleError,
   options
 );
+
+function getPublicIPAddress() {
+  fetch("http://ifconfig.me/ip")
+    .then((response) => response.text())
+    .then((data) => {
+      const ipAddress = data.trim(); // Remove any leading/trailing whitespace
+      document.getElementById("ip-address").textContent = ipAddress;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      document.getElementById("ip-address").textContent =
+        "Error fetching IP address";
+    });
+}
+
+// Call the function to get the public IP address
+getPublicIPAddress();
